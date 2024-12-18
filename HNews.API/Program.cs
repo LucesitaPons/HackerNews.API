84be215c8ee45b3f452b6dbeb259086bf57d0ffe
LucesitaPons <lucesitapons@gmail.com>
2024-12-18 06:01:31 -0600

    API functionality and license

--- HNews.API/Program.cs
+++ HNews.API/Program.cs
@@ -1,4 +1,16 @@
+using HNews.API.Service;
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Adding HTTPService HNews
+var config = builder.Configuration;
+
+builder.Services.AddHttpClient<HackerNewsApiService>(client =>
+{
+    client.BaseAddress = new Uri(config["HackerNewsApiSettings:BaseUrl"]);
+});
+
 
 // Add services to the container.
 
@@ -7,14 +19,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var corsRules = "CorsRules";
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: corsRules, builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+}
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
+
+app.UseCors(corsRules);
 
 app.UseAuthorization();
 
